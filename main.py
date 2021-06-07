f6ba02be55ccae87134abe164cee98b4ebbcdcca
Fernando Serrano <fernando.serrano@rappi.com>
2021-06-07 17:26:45 -0300

    configuring CI

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+from flask import escape
+
 def hello_world(request):
-    """Responds to any HTTP request.
+    """HTTP Cloud Function.
     Args:
-        request (flask.Request): HTTP request object.
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
     Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
-    request_json = request.get_json()
-    if request.args and 'message' in request.args:
-        return request.args.get('message')
-    elif request_json and 'message' in request_json:
-        return request_json['message']
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
     else:
-        return f'Hello Fernando!'
+        name = 'World'
+    return 'Hello {}!'.format(escape(name))
